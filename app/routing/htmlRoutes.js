33be06f9205ab3d1b74f5d45fe440af5d2dbbde8
Jonathan Meyer <jonathan.meyer@libertymutual.com>
2019-06-26 10:25:41 -0400

    form validation

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,16 @@
 const path = require("path");
 const express = require("express");
+
 const router = express.Router();
 
 router
   .use("/survey", express.static(path.resolve("app", "public", "survey.html")))
   .use("/bootstrap", express.static(path.resolve("node_modules", "bootstrap")))
   .use("/jquery", express.static(path.resolve("node_modules", "jquery")))
+  .use(
+    "/jquery-validation",
+    express.static(path.resolve("node_modules", "jquery-validation"))
+  )
 
   .use(
     "/",
